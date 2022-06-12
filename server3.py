3315853ee6899c4fca0a44599d868592bda5f255
177boris <lanreboris11@yahoo.com>
2022-06-12 23:08:27 +0100

    refactored server code: server3.py

--- server3.py
+++ server3.py
@@ -1,18 +1,4 @@
-"""
-Hello, Web. 
-
-The basic idea is simple:
-
-- Wait for someone to connect to our server and send an HTTP request; 
-- parse that request;
-- figure out what it's asking for;
-- fetch that data (or generate it dynamically);
-- format the data as HTML; and
-- send it back.
-"""
-
-
-# __author - Lanre B
+# Refactored version of server2 
 
 
 import sys, os, BaseHTTPServer
@@ -25,7 +11,30 @@ class ServerException(Exception):
 
 #-------------------------------------------------------------------------------
 
-class case_no_file(object):
+class base_case(object):
+    '''Parent for case handlers.'''
+
+    def handle_file(self, handler, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(full_path, msg)
+            handler.handle_error(msg)
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        assert False, 'Not implemented.'
+
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
+#-------------------------------------------------------------------------------
+
+class case_no_file(base_case):
     '''File or directory does not exist.'''
 
     def test(self, handler):
@@ -36,62 +45,83 @@ class case_no_file(object):
 
 #-------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------
-class case_cgi_file(object):
+class case_cgi_file(base_case):
     '''Something runnable.'''
+
+    def run_cgi(self, handler):
+        cmd = "python " + handler.full_path
+        child_stdin, child_stdout = os.popen2(cmd)
+        child_stdin.close()
+        data = child_stdout.read()
+        child_stdout.close()
+        handler.send_content(data)
 
     def test(self, handler):
         return os.path.isfile(handler.full_path) and \
                handler.full_path.endswith('.py')
 
     def act(self, handler):
-        handler.run_cgi(handler.full_path)
-
+        self.run_cgi(handler)
 
 #-------------------------------------------------------------------------------
 
-class case_existing_file(object):
+class case_existing_file(base_case):
     '''File exists.'''
 
     def test(self, handler):
         return os.path.isfile(handler.full_path)
 
     def act(self, handler):
-        handler.handle_file(handler.full_path)
+        self.handle_file(handler, handler.full_path)
 
 #-------------------------------------------------------------------------------
 
-class case_directory_index_file(object):
+class case_directory_index_file(base_case):
     '''Serve index.html page for a directory.'''
-
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
 
     def test(self, handler):
         return os.path.isdir(handler.full_path) and \
                os.path.isfile(self.index_path(handler))
 
     def act(self, handler):
-        handler.handle_file(self.index_path(handler))
+        self.handle_file(handler, self.index_path(handler))
 
 #-------------------------------------------------------------------------------
 
-class case_directory_no_index_file(object):
+class case_directory_no_index_file(base_case):
     '''Serve listing for a directory without an index.html page.'''
 
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
+    # How to display a directory listing.
+    Listing_Page = '''\
+        <html>
+        <body>
+        <ul>
+        {0}
+        </ul>
+        </body>
+        </html>
+        '''
+
+    def list_dir(self, handler, full_path):
+        try:
+            entries = os.listdir(full_path)
+            bullets = ['<li>{0}</li>'.format(e) for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            handler.send_content(page)
+        except OSError as msg:
+            msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
+            handler.handle_error(msg)
 
     def test(self, handler):
         return os.path.isdir(handler.full_path) and \
                not os.path.isfile(self.index_path(handler))
 
     def act(self, handler):
-        handler.list_dir(handler.full_path)
+        self.list_dir(handler, handler.full_path)
 
 #-------------------------------------------------------------------------------
 
-class case_always_fail(object):
+class case_always_fail(base_case):
     '''Base case if nothing else worked.'''
 
     def test(self, handler):
@@ -102,16 +132,12 @@ class case_always_fail(object):
 
 #-------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------
-
-
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-
     '''
     If the requested path maps to a file, that file is served.
     If anything goes wrong, an error page is constructed.
     '''
-    
+
     Cases = [case_no_file(),
              case_cgi_file(),
              case_existing_file(),
@@ -129,18 +155,7 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         </html>
         """
 
-    # How to display a directory listing.
-    Listing_Page = '''\
-        <html>
-        <body>
-        <ul>
-        {0}
-        </ul>
-        </body>
-        </html>
-        '''
-
-    # Classify and handle request
+    # Classify and handle request.
     def do_GET(self):
         try:
 
@@ -149,57 +164,19 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
             # Figure out how to handle it.
             for case in self.Cases:
-                handler = case()
-                if handler.test(self):
-                    handler.act(self)
+                if case.test(self):
+                    case.act(self)
                     break
 
         # Handle errors.
         except Exception as msg:
             self.handle_error(msg)
-
-    
-    # Note that we open the file in binary mode (the b in rb) so that Python wont try to help us by altering byte sequences that look like a Windows line ending.
-    # Note also that reading the whole file into memory when serving it is a bad idea in real life, 
-    # where the file might be several gigabytes of video data. 
-    # Handling that situation is outside the scope of this chapter.
-
-    # read and return content of file requested by user 
-    def handle_file(self, full_path):
-        try:
-            with open(full_path, 'rb') as reader:
-                content = reader.read()
-            self.send_content(content)
-        except IOError as msg:
-            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
-            self.handle_error(msg)
-
+            
 
     # Handle unknown objects.
     def handle_error(self, msg):
         content = self.Error_Page.format(path=self.path, msg=msg)
         self.send_content(content, 404)
-
-
-    def run_cgi(self, full_path):
-        cmd = "python " + full_path
-        child_stdin, child_stdout = os.popen2(cmd)
-        child_stdin.close()
-        data = child_stdout.read()
-        child_stdout.close()
-        self.send_content(data)
-
-
-    def list_dir(self, full_path):
-        try:
-            entries = os.listdir(full_path)
-            bullets = ['<li>{0}</li>'.format(e) 
-                for e in entries if not e.startswith('.')]
-            page = self.Listing_Page.format('\n'.join(bullets))
-            self.send_content(page)
-        except OSError as msg:
-            msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
-            self.handle_error(msg)
 
     # Send actual content.
     def send_content(self, content, status=200):
@@ -209,11 +186,9 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(content)
 
+#-------------------------------------------------------------------------------
 
-#---------------------------------------------------------------------
-
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     serverAddress = ('', 8080)
     server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
     server.serve_forever()
