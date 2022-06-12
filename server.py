b0a7289ca11b9e46347753705dffa69c8b0bcb4c
177boris <lanreboris11@yahoo.com>
2022-06-12 22:47:12 +0100

    implemented handlers and cases

--- server.py
+++ server.py
@@ -11,67 +11,95 @@ The basic idea is simple:
 - send it back.
 """
 
-import time 
-import BaseHTTPServer 
+
+import sys, os, BaseHTTPServer
+
+#-------------------------------------------------------------------------------
+
+class ServerException(Exception):
+    '''For internal error reporting.'''
+    pass
+
+#-------------------------------------------------------------------------------
 
 # __author - Lanre B
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
+    '''
+    If the requested path maps to a file, that file is served.
+    If anything goes wrong, an error page is constructed.
+    '''
     
-    # Handle HTTP requests by returning a fixed 'page '
 
-    # Page to send back.
-    Page = '''\
-<html>
-<body>
-<table border=1s>
-            <tr> <td>Header</td> <td>Value</td> </tr>
-            <tr> <td>Date and time</td> <td> {date_time} </td> </tr>
-            <tr> <td>Client host</td> <td>  {client_host} </td> </tr>
-            <tr> <td>Client port</td> <td>  {client_port} </td> </tr>
-            <tr> <td>Command</td> <td>  {command} </td> </tr>
-            <tr> <td>Path</td> <td> {path} </td> </tr>
-        </table>
-</body>
-</html>
-'''
+    # How to display an error.
+    Error_Page = """\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
 
 
-# Get requests 
-
+    # Classify and handle request
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+        try:
 
+            # Figure out what exactly is being requested.
+            full_path = os.getcwd() + self.path
 
-    def create_page(self):
-        # implement this 
-        values = {
-            'date_time'   : self.date_time_string(),
-            'client_host' : self.client_address[0],
-            'client_port' : self.client_address[1],
-            'command'     : self.command,
-            'path'        : self.path
-        } 
+            # It doesn't exist...
+            if not os.path.exists(full_path):
+                raise ServerException("'{0}' not found".format(self.path))
 
-        print(values)
+            # ...it's a file...
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
 
-        page = self.Page.format(**values)
-        return page 
+            # ...it's something we don't handle.
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+
+        # Handle errors.
+        except Exception as msg:
+            self.handle_error(msg)
 
     
+    # Note that we open the file in binary mode (the b in rb) so that Python wont try to help us by altering byte sequences that look like a Windows line ending.
+    # Note also that reading the whole file into memory when serving it is a bad idea in real life, 
+    # where the file might be several gigabytes of video data. 
+    # Handling that situation is outside the scope of this chapter.
 
-    def send_page(self, page):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(self.Page))) 
+    # read and return content of file requested by user 
+
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+
+
+    # Send actual content.
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
         self.end_headers()
-        self.wfile.write(self.Page) 
+        self.wfile.write(content)
+
+
+    # Handle unknown objects.
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
 
 
 #---------------------------------------------------------------------
-
 
 if __name__ == "__main__":
 
