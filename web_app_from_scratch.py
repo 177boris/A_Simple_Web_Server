6fe287cac2637eaba1afc5933a9e683e0887ecd8
177boris <lanreboris11@yahoo.com>
2022-06-20 00:50:21 +0100

    server can serve files

--- web_app_from_scratch.py
+++ web_app_from_scratch.py
@@ -4,31 +4,123 @@
 # Required libraries  
 import socket
 import typing 
+import mimetypes 
+import os 
+import socket 
+import typing 
+
+
+SERVER_ROOT = os.path.abspath("www")
+
+# A test response 
+RESPONSE = b"""\
+HTTP/1.1 200 OK
+Content-type: text/html
+Content-length: 15
+
+<h1>Hello!</h1>""".replace(b"\n", b"\r\n")
+
+
+BAD_REQUEST_RESPONSE = b"""\
+HTTP/1.1 400 Bad Request
+Content-type: text/plain
+Content-length: 11
+
+Bad Request""".replace(b"\n", b"\r\n")
+
+
+NOT_FOUND_RESPONSE = b"""\
+HTTP/1.1 404 Not Found
+Content-type: text/plain
+Content-length: 9
+
+Not Found""".replace(b"\n", b"\r\n")
+
+
+METHOD_NOT_ALLOWED_RESPONSE = b"""\
+HTTP/1.1 405 Method Not Allowed
+Content-type: text/plain
+Content-length: 17
+
+Method Not Allowed""".replace(b"\n", b"\r\n")
+
+FILE_RESPONSE_TEMPLATE = """\
+HTTP/1.1 200 OK
+Content-type: {content_type}
+Content-length: {content_length}
+
+""".replace("\n", "\r\n")
+
 
 HOST = "127.0.0.1"
 PORT = 9000
 
-""" 
-# By default, socket.socket creates TCP sockets.
-with socket.socket() as server_sock:
-    # This tells the kernel to reuse sockets that are in `TIME_WAIT` state.
-    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    # This tells the socket what address to bind to.
-    server_sock.bind((HOST, PORT))
 
-    # 0 is the number of pending connections the socket may have before
-    # new connections are refused.  Since this server is going to process
-    # one connection at a time, we want to refuse any additional connections.
-    server_sock.listen(0)
-    print(f"Listening on {HOST}:{PORT}...")
-"""
+def iter_lines(sock: socket.socket, bufsize: int = 16_384) -> typing.Generator[bytes, None, bytes]:
+    """Given a socket, read all the individual CRLF-separated lines
+    and yield each one until an empty one is found.  Returns the
+    remainder after the empty line.
+    """
+    buff = b""
+    while True:
+        data = sock.recv(bufsize)
+        if not data:
+            return b""
+
+        buff += data
+        while True:
+            try:
+                i = buff.index(b"\r\n")
+                line, buff = buff[:i], buff[i + 2:]
+                if not line:
+                    return buff
+
+                yield line
+            except IndexError:
+                break
+
+def serve_file(sock: socket.socket, path: str) -> None:
+    # Given a socket and the relative path to a file (relative to
+    # SERVER_SOCK), send that file to the socket if it exists.  If the
+    # file doesn't exist, send a "404 Not Found" response.
+    
+        if path == "/":
+            path = "/index.html"
+
+        abspath = os.path.normpath(os.path.join(SERVER_ROOT, path.lstrip("/")))
+        if not abspath.startswith(SERVER_ROOT):
+            sock.sendall(NOT_FOUND_RESPONSE)
+            return
+
+        try:
+            with open(abspath, "rb") as f:
+                stat = os.fstat(f.fileno())
+                content_type, encoding = mimetypes.guess_type(abspath)
+                if content_type is None:
+                    content_type = "application/octet-stream"
+
+                if encoding is not None:
+                    content_type += f"; charset={encoding}"
+
+                response_headers = FILE_RESPONSE_TEMPLATE.format(
+                    content_type=content_type,
+                    content_length=stat.st_size,
+                ).encode("ascii")
+
+                sock.sendall(response_headers)
+                sock.sendfile(f)
+        except FileNotFoundError:
+            sock.sendall(NOT_FOUND_RESPONSE)
+            return
 
 
 class Request(typing.NamedTuple):
     method: str
     path: str 
     headers: typing.Mapping[str, str]
+
+    
 
 
     @classmethod
@@ -61,46 +153,21 @@ class Request(typing.NamedTuple):
         return cls(method=method.upper(), path=path, headers=headers)
 
 
+""" 
+# By default, socket.socket creates TCP sockets.
+with socket.socket() as server_sock:
+    # This tells the kernel to reuse sockets that are in `TIME_WAIT` state.
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-def iter_lines(sock: socket.socket, bufsize: int = 16_384) -> typing.Generator[bytes, None, bytes]:
-    """Given a socket, read all the individual CRLF-separated lines
-    and yield each one until an empty one is found.  Returns the
-    remainder after the empty line.
-    """
-    buff = b""
-    while True:
-        data = sock.recv(bufsize)
-        if not data:
-            return b""
+    # This tells the socket what address to bind to.
+    server_sock.bind((HOST, PORT))
 
-        buff += data
-        while True:
-            try:
-                i = buff.index(b"\r\n")
-                line, buff = buff[:i], buff[i + 2:]
-                if not line:
-                    return buff
-
-                yield line
-            except IndexError:
-                break
-
-
-# A test response 
-RESPONSE = b"""\
-HTTP/1.1 200 OK
-Content-type: text/html
-Content-length: 15
-
-<h1>Hello!</h1>""".replace(b"\n", b"\r\n")
-
-
-BAD_REQUEST_RESPONSE = b"""\
-HTTP/1.1 400 Bad Request
-Content-type: text/plain
-Content-length: 11
-
-Bad Request""".replace(b"\n", b"\r\n")
+    # 0 is the number of pending connections the socket may have before
+    # new connections are refused.  Since this server is going to process
+    # one connection at a time, we want to refuse any additional connections.
+    server_sock.listen(0)
+    print(f"Listening on {HOST}:{PORT}...")
+"""
 
 
 # Handle connections to the server
@@ -126,10 +193,14 @@ with socket.socket() as server_sock:
         with client_sock:
             try:
                 request = Request.from_socket(client_sock)
-                print(request)
-                client_sock.sendall(RESPONSE)
+                if request.method != "GET":
+                    client_sock.sendall(METHOD_NOT_ALLOWED_RESPONSE)
+                    continue
+
+                serve_file(client_sock, request.path)
             except Exception as e:
                 print(f"Failed to parse request: {e}")
                 client_sock.sendall(BAD_REQUEST_RESPONSE)
 
 
+# Tutorial:     https://defn.io/2018/02/25/web-app-from-scratch-01/ 
